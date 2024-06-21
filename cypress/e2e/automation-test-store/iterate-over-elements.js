d0d9be02cd99e1007e8d9148f3d7795f272789de
Shirin Bashar <44616896+shirinbashar@users.noreply.github.com>
2024-06-21 01:46:03 -0400

    iterate over elements

--- cypress/e2e/automation-test-store/iterate-over-elements.js
+++ cypress/e2e/automation-test-store/iterate-over-elements.js
@@ -8,14 +8,20 @@ describe("Iterate over elements", () => {
         cy.visit("https://www.automationteststore.com/")
         cy.get("a[href*='product/category&path=']").contains("Hair Care").click()
 
-        cy.get('ul>li').each(($el, index, $list) => {
-           
+        cy.get('.fixed_wrapper .prdocutname').each(($el, index, $list) => {
+            cy.log("Index: " + index + " : " + $el.text())
         })
     });
     it("Add specific product to basket", () => {
         //Cypress Code
         cy.visit("https://www.automationteststore.com/")
         cy.get("a[href*='product/category&path=']").contains("Hair Care").click()
+
+        cy.get('.fixed_wrapper .prdocutname').each(($el, index, $list) => {
+            if($el.text().includes('Curls to straight Shampoo')){
+                cy.wrap($el).click()
+            }
+        })
 
 
 
